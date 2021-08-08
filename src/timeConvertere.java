d20b37028f2397a8dd51e42391711cddcd423c54
Hridhin Pt <hridhinplathottathil@gmail.com>
2021-08-08 23:04:25 +0530

    full

--- src/timeConvertere.java
+++ src/timeConvertere.java
@@ -1,31 +1,26 @@
 public class timeConvertere {
     public static String getDurationString(int minutes, int seconds){
-        if(minutes <=0 || seconds <=0 || seconds >=60){
+        if(minutes <0 || seconds <0 || seconds >=60){
             return ("Invalid");
         }
         int hours = minutes/60;
         int minute =minutes %60;
-        int sec = seconds;
-        String result = hours + " hours "+ minute +" mintes "+ sec + " seconds ";
-        return result;
+        return hours + " h "+ minute +" m "+ seconds + " s ";
 
     }
-    public static void getDurationString( int seconds){
-        if (seconds <= 0){
-            System.out.println("Invalid");
+    public static String getDurationString( int seconds){
+        if (seconds < 0){
+            return "Invalid";
         }else{
-            int min = seconds/60;
-            System.out.println("Minutes Calculated :"+ min);
-            String passedResult = getDurationString(min,seconds);
-            System.out.println("Passed result "+ passedResult);
-
+            int minutes = seconds/ 60;
+            int sec = seconds % 60;
+            return  (getDurationString(minutes,sec));
         }
-
     }
 
     public static void main(String[] args) {
-        System.out.println(getDurationString(61,1));
+        System.out.println(getDurationString(65,45));
+        System.out.println(getDurationString(3945));
 
-        getDurationString(219601);
     }
 }
